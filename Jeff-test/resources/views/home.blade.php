beacdb0bf09269925248b104a212181f80cb9986
Nour-keswani <you@example.com>
2022-02-25 11:50:04 +0100

    Version 1.0.0

---
+++ Jeff-test/resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://preview.redd.it/as6lqng25u161.png?width=640&crop=smart&auto=webp&s=be74789b777ede2ae2666834e9c08a58c98d7c13" class="rounded-circle" style="width: 100px; height: 100px">
+        </div>
+        <div class="col-9">
+            <div><h1>Jeff&Betty</h1></div>
+            <div class="d-flex">
+                <div class="pe-4"><strong>253</strong> Posts</div>
+                <div class="pe-4"><strong>28K</strong> Followers</div>
+                <div class="pe-4"><strong>512K</strong> Following</div>
+            </div>
+            <div class="pt-4 fw-bold">Jeff&Betty.nl</div>
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus facere blanditiis repellat labore veritatis, nisi atque. Deserunt hic dolor tempora repellendus quasi non tenetur in, dolore quaerat excepturi.</div>
+            <div><a href="#">www.jeff&betty.nl</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://www.avso.org/wp-content/uploads/files/5/3/1/the-car-garage-arrange-some-practical-advice-facility-0-531.jpg" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://www.augustgarage.ca/wp-content/uploads/2020/02/Vehicle-Modification-and-Customization-at-August-Garage-in-Kelowna-BC.jpg" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://cdn11.bigcommerce.com/s-48ae1/product_images/uploaded_images/bendpak4postlift84.jpg" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
